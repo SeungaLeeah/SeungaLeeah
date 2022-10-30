08c574e9f40862d800a6fd1b5504fc5266fa5f02
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-10-31 00:01:34 +0900

    vue

---
+++ practice/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,25 @@
+<!-- 문자열 바인딩 -->
+<template>
+    <div>
+        <h1 class="testColor">{{userName}}</h1>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Leeah',
+      message: 'Welcome 개발자 leeah',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+<style scoped>
+.testColor{
+    color: red;
+}
+</style>
